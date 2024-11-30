da71489c5e43926701cc9d6fb86b2821327ea07d
Nileshchaurasiaa <144258809+Nileshchaurasiaa@users.noreply.github.com>
2024-11-30 21:39:24 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+import { useState } from "react";
+import "./App.css";
+import Layout from "./layout/Layout";
+function App() {
+  return <Layout />;
+}
+
+export default App;
